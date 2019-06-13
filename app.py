2843f961cd53b39c737582f74300d3b592200afd
PsyKzz <matt.daemon660@gmail.com>
2019-06-14 00:31:42 +0100

    Cleanup messages

--- app.py
+++ app.py
@@ -14,14 +14,31 @@ def index():
     g = Github(gh_token)
     repo = g.get_repo(f'{repo_org}/{repo_name}')
     prs = request.args.get('prs','').split(',')
-    for pr in prs:
-        if not pr or not request.query_string:
-            continue
-        add_comment(repo, pr, request.query_string)
+    if not prs:
+        return "NOT OK"
+    full_prs = [repo.get_pull(int(pr)) for pr in prs]
+    for pr in full_prs:
+        add_comment(repo, pr, full_prs, request.query_string, request.args)
         
     return "OK"
 
-def add_comment(repo, pr_number, data):
-    print(f"repo: {repo}, pr: {pr_number}, data: {data}")
-    pr = repo.get_pull(int(pr_number))
-    pr.create_issue_comment(f'Test Comment, raw data: {data}')
+def add_comment(repo, pr, full_prs, raw_data, req_args):
+    print(f"raw_data: {raw_data}")
+
+    round_id = req_args.get('roundId', '-1')
+    unique_runtimes = req_args.get('uniqueRuntimes', '-1')
+    total_runtimes = req_args.get('totalRuntimes', '-1')
+    server_commit = req_args.get('commit', 'unknown')
+    prs = req_args.get('prs', '').split(',')
+
+    pr_list = [f"{pr.url} ({pr.title})" for pr in full_prs]
+
+    message = f"""
+Round {round_id} @ {commit}
+=====
+Runtimes: {unique_runtimes} ({total_runtimes} total)
+- {pr_list.join('\n-')}
+    """
+
+
+    pr.create_issue_comment(message)
