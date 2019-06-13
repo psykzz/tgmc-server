a0f91f618856fed850bf5d60f03a6f70b8857d6a
PsyKzz <matt.daemon660@gmail.com>
2019-06-14 00:03:51 +0100

    logs and shit

--- app.py
+++ app.py
@@ -15,10 +15,13 @@ def index():
     repo = g.get_repo(f'{repo_org}/{repo_name}')
     prs = request.args.get('prs','').split(',')
     for pr in prs:
+        if not pr or not request.query_string:
+            continue
         add_comment(repo, pr, request.query_string)
         
     return "OK"
 
 def add_comment(repo, pr_number, data):
-    pr = repo.get_pull(pr_number)
+    print(f"repo: {repo}, pr: {pr_number}, data: {data}")
+    pr = repo.get_pull(int(pr_number))
     pr.create_issue_comment(f'Test Comment, raw data: {data}')
