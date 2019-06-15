c485eda0fb1e83dface2f55ba09c5c0bee64400c
PsyKzz <matt.daemon660@gmail.com>
2019-06-15 19:37:17 +0100

    debug printthebody

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def add_comment(repo, pr, full_prs, raw_data, req_args):
 
     # Check if a comment already exists
     for comment in pr.get_issue_comments():
+        print(comment.body)
         if "Round {round_id}" in comment.body:
             print(f"Skipping, round {round_id} has already been processed for this PR.")
             return
